199a805f7c49f3606b30ef1fdd17c1486256cc2f
kouji2004 <mrtaxi48@gmail.com>
2022-12-23 12:11:11 +0000

    itemsコントローラーを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+
+
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kata, :first_name_kata, :post_code, :address, :phone_number, :email])
+  end
+
 end
