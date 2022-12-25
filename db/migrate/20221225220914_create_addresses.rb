443fc76fa060de28ab7c1235a04b7706b159061e
kouji2004 <mrtaxi48@gmail.com>
2022-12-25 22:09:54 +0000

    モデルの作成

---
+++ db/migrate/20221225220914_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+
+      t.integer :customer_id, null: false
+      t.string :post_code, null: false
+      t.string :address, null: false
+      t.string :address_name, null: false
+
+      t.timestamps
+    end
+  end
+end
