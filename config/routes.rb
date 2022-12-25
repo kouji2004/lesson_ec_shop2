d63ae31864b953d44b7685874312a6a7eec9863d
kouji2004 <mrtaxi48@gmail.com>
2022-12-25 22:13:26 +0000

    ホームコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'homes/about'
+    get 'homes/top'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
