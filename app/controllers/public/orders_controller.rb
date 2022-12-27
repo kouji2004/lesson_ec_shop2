32e4297efd31072ffb870e8aa23d5bbec66a6362
kouji2004 <mrtaxi48@gmail.com>
2022-12-27 12:03:03 +0000

    注文確定ボタンをクリック後に tahanks画面に遷移する

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -26,6 +26,29 @@ class Public::OrdersController < ApplicationController
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
   end
 
+ def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    @order.order_status = 0
+    if @order.save
+      cart_items.each do |cart|
+        order_detail = OrderDetail.new
+        order_detail.item_id = cart.item_id
+        order_detail.order_id = @order.id
+        order_detail.item_count = cart.item_count
+        order_detail.ordered_price = cart.item.price
+        order_detail.making_status = 0
+        order_detail.save
+      end
+      redirect_to thanks_path
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
+ end
+
+
   def new
     @items = Item.all
      @order = Order.new
