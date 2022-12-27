91dad6f077a245e8581e43800a7347870236bd7b
kouji2004 <mrtaxi48@gmail.com>
2022-12-27 11:57:36 +0000

    注文情報確定画面の作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,6 +3,27 @@ class Public::OrdersController < ApplicationController
   end
 
   def log
+    @order = Order.new(order_params)
+    @order.postage = 800
+    if params[:order][:address_number] == "1"
+      @order.address = current_customer.address
+      @order.post_code = current_customer.post_code
+      @order.address_name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_number] == "2"
+      @address = Address.find(params[:order][:registered])
+      @order.address = @address.address
+      @order.post_code = @address.post_code
+      @order.address_name = @address.address_name
+      @order.payment_method = params[:order][:payment_method]
+    elsif params[:order][:address_number] == "3"
+      address_new = current_customer.addresses.new(address_params)
+      if address_new.save
+      else
+        render :new
+      end
+    end
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
   end
 
   def new
@@ -16,4 +37,11 @@ class Public::OrdersController < ApplicationController
 
   def thanks
   end
+
+    private
+
+  def order_params
+    params.require(:order).permit(:address_name, :postage, :billing_amount, :address, :payment_method,:post_code, :post_code)
+  end
+
 end
